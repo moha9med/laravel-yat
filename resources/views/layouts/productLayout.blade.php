1c883b708e280379c4bce2f48a5ddf2d6ebaaa11
mohamed saeed <moh.a9.said@gmail.com>
2021-02-17 21:27:20 +0200

    modify menu || add categories item

--- resources/views/layouts/productLayout.blade.php
+++ resources/views/layouts/productLayout.blade.php
@@ -30,6 +30,10 @@
                     <li class="nav-item">
                         <a class="nav-link {{ Request::fullUrl() == route('addProducts') ?'active':'' }}" href="{{route('addProducts')}}">Add Product</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link {{ Request::fullUrl() == route('allcategories') ?'active':'' }}" href="{{route('allcategories')}}">Categories</a>
+                    </li>
+                    
 
                 </ul>
             </div>
