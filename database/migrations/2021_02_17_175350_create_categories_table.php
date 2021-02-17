b797697a489be4ade7d420a4b420d971ee3f8db3
mohamed saeed <moh.a9.said@gmail.com>
2021-02-17 20:02:36 +0200

    cateogy migration file

--- database/migrations/2021_02_17_175350_create_categories_table.php
+++ database/migrations/2021_02_17_175350_create_categories_table.php
@@ -15,6 +15,8 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('title',255);
+            $table->text('description');
             $table->timestamps();
         });
     }
